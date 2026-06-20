b506d59ed49e759fe88e534fe600c617d5c5c6e8
dekema9924 <danekema9924@gmail.com>
2026-06-20 12:15:20 -0700

    cors error

--- config/config.tsx
+++ config/config.tsx
@@ -1,5 +1,5 @@
 
-
-export const BaseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://resume-bvz2lc0ft-dekema9924s-projects.vercel.app"
-
-
+export const BaseUrl =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : ""
